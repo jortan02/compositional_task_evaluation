229298e6bc1da3782224168dfed8a2c12a2d2cc1
Jordan Tan <jortan2002@gmail.com>
2023-08-31 18:14:36 -0600

    Added reasons for multiplication

--- generate_summary.py
+++ generate_summary.py
@@ -39,6 +39,7 @@ def multplication_experiment(model_name, results_list):
         model=model_name,
         prompt="Carry",
         file_path=f"{CARRY_PATH}/{model_name}_carry.csv",
+        reason="Carry occurs in multiplication at some digits place is greater than 9."
     )
 )
     results_list.append(
@@ -46,6 +47,7 @@ def multplication_experiment(model_name, results_list):
         model=model_name,
         prompt="Concatenate",
         file_path=f"{CONCATENATE_PATH}/{model_name}_concatenate.csv",
+        reason="The digits needs to be concatenated to the final answer."
     )
 )
     results_list.append(
@@ -53,6 +55,7 @@ def multplication_experiment(model_name, results_list):
         model=model_name,
         prompt="Multiply",
         file_path=f"{MULTIPLY_PATH}/{model_name}_multiply.csv",
+        reason="Multiplication is the compositional task."
     )
 )
     results_list.append(
@@ -60,6 +63,7 @@ def multplication_experiment(model_name, results_list):
         model=model_name,
         prompt="Multiply 1-digit",
         file_path=f"{MULTIPLY_1_DIGIT_PATH}/{model_name}_multiply_1_digit.csv",
+        reason="Multiplication requires multiplying two values at some digits place."
     )
 )
     results_list.append(
@@ -67,6 +71,7 @@ def multplication_experiment(model_name, results_list):
         model=model_name,
         prompt="Sum",
         file_path=f"{SUM_PATH}/{model_name}_sum.csv",
+        reason="Summation is done after carrying occurs."
     )
 )
 
